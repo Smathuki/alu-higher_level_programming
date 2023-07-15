0242004844586ca12e7d7b7c7a57f89836dbb4e4
alu-higher_level_programming <stonemathuki@gmail.com>
2023-07-15 08:32:18 -0700

    replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for num in range(len(new_list)):
-       if new_list[num] == search:
-           new_list[num] = replace
-    return(new_list)
+        if new_list[num] == search:
+            new_list[num] = replace
+    return new_list
