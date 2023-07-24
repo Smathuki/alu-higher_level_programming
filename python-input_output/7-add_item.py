b4d68aab35ffa41e88e1dcb9d66bebe0502ad95b
Smathuki <85058318+Smathuki@users.noreply.github.com>
2023-07-24 13:28:46 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,7 @@ from typing import List
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 def add_items_to_list_and_save(args: List[str]):
     """
     Add all arguments to a Python list and save them to a file in JSON format.
