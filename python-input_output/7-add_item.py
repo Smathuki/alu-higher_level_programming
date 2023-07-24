c517a4c6fd0f9eedfb19491efc061468e4eaff7d
Smathuki <85058318+Smathuki@users.noreply.github.com>
2023-07-24 13:25:37 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file:"""
-
-
 import sys
 import os.path
-
 
 from typing import List
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
