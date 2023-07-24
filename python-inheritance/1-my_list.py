845ea453a8e7ea56d91a5cf67f2b3192ddfc29dd
Smathuki <85058318+Smathuki@users.noreply.github.com>
2023-07-24 12:32:50 +0200

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-'''This file creates a class that inherits from List builtin functions '''
-
 
 class MyList(list):
-    '''This Class inherits the built in function list'''
+    """
+    This class inherits from the built-in list class and provides additional functionality.
+    """
 
     def print_sorted(self):
-        '''Prints the list in a sorted order  '''
-        sorted_list = self[:]
-        sorted_list.sort()
-        print("{}".format(sorted_list))
+        """
+        Print the list in sorted order (ascending).
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
